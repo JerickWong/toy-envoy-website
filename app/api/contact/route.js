10f07f06d56b3a69c920679696fdbab9178d35d9
JerickWong <jerick_wong@dlsu.edu.ph>
2023-05-30 19:20:57 +0800

    Fix async sending of email

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -24,21 +24,12 @@ export async function POST(req) {
     text: message,
   };
 
-  transporter.verify(function(error, success) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Server is ready to take our messages');
-    }
-  });
+  const isVerified = await transporter.verify()
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-      return NextResponse.json({ error: 'Error sending email' }, { status: error.status });
-    } else {
-      console.log('Email sent: ' + info.response);
-      return NextResponse.json({ message: 'Email sent successfully' });
-    }
-  });
+  if (!isVerified) {
+    return NextResponse.json({ error: 'Error sending email' }, { status: 400 });
+  }
+
+  await transporter.sendMail(mailOptions)
+  return NextResponse.json({ message: 'Email sent successfully' })
 }
