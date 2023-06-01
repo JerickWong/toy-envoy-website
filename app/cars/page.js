6c0094bb7ac33d251827f84c9893bbe3e61d3ea3
JerickWong <jerick_wong@dlsu.edu.ph>
2023-06-01 21:57:00 +0800

    Slight responsive detailed page

--- app/cars/page.js
+++ app/cars/page.js
@@ -3,27 +3,67 @@
 import { useRouter } from 'next/navigation'
 import Image from 'next/image'
 import SecondaryNavbar from '@/components/SecondaryNavbar'
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
 
 export default function Home() {
   const router = useRouter()
+  const [sliderRef] = useKeenSlider({
+    loop: true,
+    slidesPerView: 1,
+    mode: 'free-snap',
+    spacing: 15,
+  })
 
   return (
     <div className='max-h-screen min-h-screen flex flex-col'>
       <SecondaryNavbar />
-      <section className='my-4 mx-9 flex justify-center gap-4'>
-        <div>
-          <Image
-            sizes='(max-width: 765px) 100vw, 500px'
-            srcset='
-            /vintage-item1-300.jpg 300w,
-            /vintage-item1-765.jpg 765w'
-            width={100}
-            height={10}
-            src='/vintage-item1-765.jpg'
-            className='w-full'
-            alt='Vintage Car' />
+      <section className='md:flex-row flex-col my-4 mx-9 flex justify-center gap-4'>
+        <div ref={sliderRef} className='keen-slider slider'>
+          <div className='keen-slider__slide'>
+            <div>
+              <Image
+                sizes='(max-width: 765px) 100vw, 500px'
+                srcset='
+                /vintage-item1-300.jpg 300w,
+                /vintage-item1-765.jpg 765w'
+                width={100}
+                height={10}
+                src='/vintage-item1-765.jpg'
+                className='w-full object-cover'
+                alt='Vintage Car' />
+            </div>
+          </div>
+          <div className='keen-slider__slide'>
+            <div>
+              <Image
+                sizes='(max-width: 765px) 100vw, 500px'
+                srcset='
+                /vintage-item1-300.jpg 300w,
+                /vintage-item1-765.jpg 765w'
+                width={100}
+                height={10}
+                src='/vintage-item1-765.jpg'
+                className='w-full object-cover'
+                alt='Vintage Car' />
+            </div>
+          </div>
+          <div className='keen-slider__slide'>
+            <div>
+              <Image
+                sizes='(max-width: 765px) 100vw, 500px'
+                srcset='
+                /vintage-item1-300.jpg 300w,
+                /vintage-item1-765.jpg 765w'
+                width={100}
+                height={10}
+                src='/vintage-item1-765.jpg'
+                className='w-full object-cover'
+                alt='Vintage Car' />
+            </div>
+          </div>
         </div>
-        <div className='flex flex-col max-w-40-screen font-montserrat'>
+        <div className='content font-montserrat'>
           <h2 className='text-4xl mb-1 font-extrabold dark:text-white '>Brand X Vintage Car</h2>
           <h3 className='text-3xl mb-2 font-light dark:text-white '>$6000.00</h3>
           <button type='button' className='button-interested ' onClick={() => router.push('/contact')}>
@@ -39,8 +79,8 @@ export default function Home() {
           </p>
         </div>
       </section>
-      <hr className='my-2 border-t-2 border-black' />
-      <div className='flex justify-evenly gap-4'>
+      <hr className='md:block hidden my-2 border-t-2 border-black' />
+      <div className='md:flex justify-evenly gap-4 hidden'>
         <Image
           sizes='(max-width: 765px) 100vw, 400px'
           srcset='
